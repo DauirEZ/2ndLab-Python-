bc71b0b8ab9086069e0a6c00050c2c2968ddb6aa
Dauir <mm0222125@gmail.com>
2023-02-03 21:38:13 +0600

    While_Loops

---
+++ Python_While_Loops/4.py
@@ -0,0 +1,8 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+  
+#With the else statement we can run a block of code once when the condition no longer is true
